941824025cbd691497ae8d98fc00e9717db81b44
ChuckmonsterXlx <alfonso_309@hotmail.com>
2023-06-15 12:01:55 -0700

    token creation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt";
 
-export const register = (req: Request, res: Response) => {
-  console.log(req.body);
+export const register = async (req: Request, res: Response) => {
+  const { userName, email, password } = req.body;
 
-  res.send("Registrando");
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      userName: userName,
+      email: email,
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+    res.send({
+      id: userSaved._id,
+      userName: userSaved.userName,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = (_req: Request, res: Response) => {
