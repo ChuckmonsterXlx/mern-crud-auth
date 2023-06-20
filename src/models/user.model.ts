8fdb48e69425af882c69c5306de98bd67bdd2206
ChuckmonsterXlx <alfonso_309@hotmail.com>
2023-06-19 18:33:14 -0700

    data validation feature finished

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    userName: { type: String, require: true, trim: true },
+    username: { type: String, require: true, trim: true },
     email: { type: String, require: true, trim: true, unique: true },
     password: { type: String, require: true },
   },
