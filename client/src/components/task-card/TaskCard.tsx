0e70779f1cee4cb96e98c148bbe486423a7a6eb4
ChuckmonsterXlx <alfonso_309@hotmail.com>
2023-10-04 23:13:22 -0700

    added btn container, and added fontawesomeIcon

--- client/src/components/task-card/TaskCard.tsx
+++ client/src/components/task-card/TaskCard.tsx
@@ -1,34 +1,48 @@
 import { useTasks } from "../../context/TasksContext";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { useState } from "react";
 dayjs.extend(utc);
 
 const TaskCard = ({ task }: { task: any }) => {
   const { deleteTask } = useTasks();
+  const [btnAreVisible, setBtnAreVisible] = useState(false);
 
   return (
-    <div className="w-full max-w-md p-10 rounded-md bg-zinc-800">
+    <div className="w-full p-10 rounded-md bg-zinc-800">
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex items-center gap-2">
-          <button
-            className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600"
-            onClick={() => deleteTask(task._id)}
+          <div
+            onClick={() => setBtnAreVisible(!btnAreVisible)}
+            className="px-3 py-1 border border-white rounded-md cursor-pointer"
           >
-            Delete
-          </button>
-          <Link
-            className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
-            to={`/tasks/${task._id}`}
-          >
-            Edit
-          </Link>
+            <FontAwesomeIcon icon={faEllipsisH} />
+          </div>
+          {btnAreVisible ? (
+            <div className="absolute flex flex-col gap-2 mt-40">
+              <Link
+                className="px-3 py-2 text-center text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                to={`/tasks/${task._id}`}
+              >
+                Edit
+              </Link>
+              <button
+                className="px-3 py-2 text-center text-white bg-red-500 rounded-md hover:bg-red-600"
+                onClick={() => deleteTask(task._id)}
+              >
+                Delete
+              </button>
+            </div>
+          ) : null}
         </div>
       </header>
-      <p className="text-slate-300">{task.description}</p>
-      <p>{dayjs(task.date).utc().format("DD/MM/YYYY")}</p>
+      <p className="mt-5 text-slate-300">{task.description}</p>
+      <p className="mt-10">{dayjs(task.date).utc().format("DD/MM/YYYY")}</p>
     </div>
   );
 };
